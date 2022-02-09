ef70ace4cbd689b1ba889748dffb61d7dfe61cf6
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-02-09 17:46:03 +0200

    Update FilterBookList.jsx

--- src/components/FilterBookList.jsx
+++ src/components/FilterBookList.jsx
@@ -1,42 +1,48 @@
 import { InputGroup, FormControl, Col, Card, Button } from "react-bootstrap";
 import { useState } from "react";
-import books from '../data/fantasy.json'
+import books from "../data/fantasy.json";
 
-const FilterBookList = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  return (
-    <InputGroup className="mb-3">
-      <FormControl
-        placeholder="Book Name"
-        onChange={(e) => {
-            setSearchTerm(e.target.value)
-        }}
-        aria-label="Recipient's username"
-        aria-describedby="basic-addon2"
-      />
-      
-      {books.filter((value) => {
-          if (searchTerm ==""){
-              return value
-          } else if (value.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-              return value
-          }
-      }).map((value, key) => {
-          return(
-            
-              <Col xs={12} md={4} lg={3} >
-              <Card key={value.asin}>
-                <Card.Img variant="top" src={value.img} />
-                <Card.Body>
-                  <Card.Title>{value.title}</Card.Title>
-                  <Card.Text>{value.price}</Card.Text>
-                  <Button variant="primary">{value.category}</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-      )} )}
-      <InputGroup.Text id="basic-addon2"></InputGroup.Text>
-    </InputGroup>
-  )}
+<div>
+  const FilterBookList = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Book Name"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+  
+        {books
+          .filter((value) => {
+            if (searchTerm === "") {
+              return value;
+            } else if (
+              value.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((value, key) => {
+            return (
+              <Col xs={12} md={4} lg={3}>
+                <Card key={value.asin}>
+                  <Card.Img variant="top" src={value.img} />
+                  <Card.Body>
+                    <Card.Title>{value.title}</Card.Title>
+                    <Card.Text>{value.price}</Card.Text>
+                    <Button variant="primary">{value.category}</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+      </InputGroup>
+    );
+  };
+</div>
 
-export default FilterBookList
+export default FilterBookList;
