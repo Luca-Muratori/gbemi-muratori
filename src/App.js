cf338b989f0309c5c43969f999947907d5eb92b3
Lucai994 <luca.muratori.94@hotmail.it>
2022-02-09 15:19:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import BadgeComponent from "./components/BadgeComponent";
 import WarningSignComponent from "./components/WarningSignComponent";
+import SingleBookComponent from "./components/SingleBookComponent";
 
 function App() {
   return (
     <div className="App">
       <WarningSignComponent />,
       <BadgeComponent />
+      <SingleBookComponent />
     </div>
   );
 }
